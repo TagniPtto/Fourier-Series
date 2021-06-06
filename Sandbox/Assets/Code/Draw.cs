ca052ca6e42f07904e1c4675877ec7f5089cc7a4
FlameThrower <geudens.tagni@gmail.com>
2021-06-06 23:00:41 +0200

    Uploaded The Project
    First upload

---
+++ Sandbox/Assets/Code/Draw.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw : MonoBehaviour
+{
+    public Camera cam;
+    public List<Vector2> DrawingPositions;
+    // Start is called before the first frame update
+    public bool CanDraw = true;
+    public float Scale = 1;
+    public LineRenderer lr;
+
+    public float maxSpaceBetweenPoints = 0.1f;
+    public Vector3 cursoroffset;
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        
+        lr.startColor = Color.black;
+        lr.endColor = Color.black;
+
+        lr.startWidth = 0.1f;
+        lr.endWidth = 0.1f;
+        DrawingPositions = new List<Vector2>();
+        
+    }
+    public void Reset()
+    {
+        DrawingPositions.Clear();
+        lr.positionCount = DrawingPositions.Count;
+        CanDraw = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Reset();
+            Vector3 p = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane));
+            //py goes from 0.8 - 1.167 or something
+            p.y -= 0.985f;
+
+
+            p *= 5 * Scale;
+            p -= cursoroffset;
+            DrawingPositions.Add(p);
+            lr.positionCount = DrawingPositions.Count;
+
+            lr.SetPosition(0, DrawingPositions[DrawingPositions.Count - 1]);
+        }
+        if (CanDraw && (Input.GetKey(KeyCode.Mouse0)))
+        {
+            Vector3 p = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane));
+            //py goes from 0.8 - 1.167 or something
+            p.y -= 0.985f;
+
+
+            p *= 5 * Scale;
+            p -= cursoroffset;
+            //Debug.Log(p.x+ "," + p.y + "," + p.z);
+
+            float x = p.x - DrawingPositions[DrawingPositions.Count - 1].x;
+            float y = p.y - DrawingPositions[DrawingPositions.Count - 1].y; 
+            float dist = Mathf.Sqrt(y*y +(x*x));
+            if (dist >=maxSpaceBetweenPoints)
+            {
+                DrawingPositions.Add(p);
+                lr.positionCount = DrawingPositions.Count;
+
+                lr.SetPosition(DrawingPositions.Count - 1, DrawingPositions[DrawingPositions.Count - 1]);
+            }
+           
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            CanDraw = false;
+        }
+
+    }
+}
---
+++ Sandbox/Assets/Drawing.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public LineRenderer lr;
+    public Fourier fourierscript;
+    int count = 0;
+    private List<Vector3> positions;
+    void Start()
+    {
+        if(lr == null)
+        {
+            lr = gameObject.GetComponent<LineRenderer>();
+        }
+        if (lr == null)
+        {
+            lr = gameObject.AddComponent<LineRenderer>();
+        }
+        count = 0;
+        lr.startColor = Color.blue;
+        lr.endColor = Color.blue;
+        lr.startWidth = 0.1f;
+        lr.endWidth = 0.1f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lr.positionCount = count + 1;
+        if (fourierscript.OrderCircles[fourierscript.OrderCircles.Length - 1] == null) return;
+        lr.SetPosition(count, new Vector3(fourierscript.OrderCircles[fourierscript.OrderCircles.Length-1].Top.x, fourierscript.OrderCircles[fourierscript.OrderCircles.Length-1].Top.y, 0.1f));
+        count++;
+
+        if (count >= (int)((1/(fourierscript.scalar*0.01)))) count = 0;
+    }
+}
